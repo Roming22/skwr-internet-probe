6662617790cdfdb3ae4ec0a6df5805002f5cf3bf
Roming22 <roming22@gmail.com>
2019-10-19 14:57:44 -0400

    Send email on reconnection

--- configure.sh
+++ configure.sh
@@ -1,3 +1,49 @@
 #!/bin/bash
 SCRIPT_DIR=`cd $(dirname $0); pwd`
-chmod g+w $SCRIPT_DIR/volumes/data
+
+SECRET="$SCRIPT_DIR/etc/secret.env"
+[[ -e "$SECRET" ]] && source <(sed -e 's:=:=":' -e 's:$:":' $SECRET)
+
+set_mandatory_value(){
+	PROMPT=$2
+	VAR=$1
+	[[ -n "${!VAR}" ]] && DEFAULT="${!VAR}" || unset DEFAULT
+	read -p "$PROMPT`[[ -n "$DEFAULT" ]] && echo -e " [$DEFAULT]"`: " INPUT
+	export $VAR="${INPUT:-$DEFAULT}"
+	[[ -z "${!VAR}" ]] && echo "Invalid value: Do not leave blank" && exit 1
+	VAR_LIST="$VAR_LIST $VAR"
+	unset INPUT
+}
+
+set_mandatory_password(){
+	VAR=$1
+	PROMPT=$2
+	[[ -n "${!VAR}" ]] && DEFAULT="${!VAR}" || unset DEFAULT
+	read -s -p "$PROMPT`[[ -n "$DEFAULT" ]] && echo -e " [leave blank to keep the current value]"`: " INPUT
+	echo
+	export $VAR="${INPUT:-$DEFAULT}"
+	[[ -z "${!VAR}" ]] && echo "Invalid value: Do not leave blank" && exit 1
+	VAR_LIST="$VAR_LIST $VAR"
+	unset INPUT
+}
+
+write_secret(){
+    VAR_LIST=( $VAR_LIST )
+    for VAR in ${VAR_LIST[@]}; do
+        echo "$VAR=${!VAR}"
+    done > $SECRET
+}
+
+create_secret(){
+	set_mandatory_value MAIL_FROM "Enter the sender email"
+	set_mandatory_value MAIL_TO "Enter the recipient email"
+	set_mandatory_password SENDGRID_API_KEY "Enter the API key for SendGrid"
+	write_secret
+}
+
+setup_volume(){
+    chmod g+w $SCRIPT_DIR/volumes/data
+}
+
+create_secret
+setup_volume
--- image/module/bin/probe.py
+++ image/module/bin/probe.py
@@ -1,7 +1,9 @@
+import os
 import datetime as dt
 import http.client as http
 from time import sleep
-
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def get_now():
   return dt.datetime.now().replace(microsecond=0)
@@ -18,6 +20,17 @@ def check_connection():
   conn.close()
   return connected
 
+def send_mail(body):
+  message = Mail(
+    from_email=os.environ.get("MAIL_FROM"),
+    to_emails=os.environ.get("MAIL_TO"),
+    subject="Internet reconnected",
+    plain_text_content=body)
+  try:
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    sg.send(message)
+  except Exception as e:
+    print(e.message)
 
 class Connection():
   def __init__(self):
@@ -25,8 +38,8 @@ class Connection():
       with open("/opt/module/data/logs.csv", "r") as logs:
         last_row = logs.readlines()[-1].split(";")
         self.start_time = dt.datetime.strptime(last_row[0], "%Y-%m-%d %H:%M:%S")
-        self.connected = bool(last_row[1])
-    except:
+        self.connected = bool(int(last_row[1]))
+    except Exception as ex:
       self.start_time = get_now()
       self.connected = check_connection()
       with open("/opt/module/data/logs.csv", "a") as logs:
@@ -37,16 +50,19 @@ class Connection():
   def log(self, now, duration):
     with open("/opt/module/data/logs.csv", "a") as logs:
       logs.write("{};\n{};{};".format(duration, now, "1" if self.connected else "0"))
+    if self.connected:
+      send_mail("Internet was disconnected for {}".format(duration))
 
 
   def monitor(self):
+    print("Last know status: {}connected".format("" if self.connected else "dis"))
     while True:
       now = get_now()
       if self.connected != check_connection():
         duration = now - self.start_time
         print("Internet was {}connected for {}.".format("" if self.connected else "dis" , duration))
-        self.start_time = now
         self.connected = not self.connected
+        self.start_time = now
         self.log(now, duration)
       if self.connected:
         sleep(10)
